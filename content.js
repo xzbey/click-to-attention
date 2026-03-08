2c4f97d6605000e915bb717c56b0e09ad707f72d
xzbey <O_O_X_X_2006@mail.ru>
2026-03-08 17:56:52 +0300

    8.3.25

--- content.js
+++ content.js
@@ -1,9 +1,29 @@
 (function() {
+    function clickAtCoordinates(x, y) {
+        const element = document.elementFromPoint(x, y);
+        if (element) {
+            const clickEvent = new MouseEvent('click', {
+                view: window,
+                bubbles: true,
+                cancelable: true,
+                clientX: x,
+                clientY: y
+            });
+            element.dispatchEvent(clickEvent);
+        }
+    }
+
     function checkAndClick() {
         let modal = document.querySelector('[data-testid="AttentionControlModal"]');
         let button = document.querySelector('[data-testid="AttentionControlModal.action.submit.Button"]');
 
-        if (modal && button) { button.click(); }
+        if (modal && button) { 
+            button.click(); 
+            
+            setTimeout(() => {
+                clickAtCoordinates(0, window.innerHeight / 2);
+            }, 10000);
+        }
 
     }
     setInterval(checkAndClick, 30000);
